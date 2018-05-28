0043cd5595e5f76e1cd100d02acdf4fc7cdc500d
Hassan Eid <hmeid@ucsd.edu>
2018-05-27 18:45:32 -0700

    changed pin count to 20

--- python/examples/strandtest.py
+++ python/examples/strandtest.py
@@ -10,10 +10,10 @@ from neopixel import *
 import argparse
 
 # LED strip configuration:
-LED_COUNT      = 300      # Number of LED pixels.
+LED_COUNT      = 20      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 1000000  # LED signal frequency in hertz (usually 800khz)
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 100     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
